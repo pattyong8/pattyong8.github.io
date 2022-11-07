e5f1deb6d738feff21483c3f12d12d765ae9b52e
Patrick Ong <patrickong@patricks-mbp.lan>
2022-11-06 18:20:15 -0800

    11.6.22

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -90,7 +90,7 @@ $(document).ready(function(){
 
         function makeTimer() {
 
-                var endTime = new Date("October 29, 2021 12:00:00 PDT");
+                var endTime = new Date("January 20, 2023 12:00:00 PDT");
                 var endTime = (Date.parse(endTime)) / 1000;
 
                 var now = new Date();
